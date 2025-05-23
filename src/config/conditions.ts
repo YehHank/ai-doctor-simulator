9b078daa366d949934f8e7df95486807c2913a1b
葉欣翰 <f127stellvia@gmail.com>
2025-05-23 07:41:16 +0000

    幫我擴充病名 結合台灣百大病症

--- src/config/conditions.ts
+++ src/config/conditions.ts
@@ -10,8 +10,29 @@ export const medicalConditions: string[] = [
   "偏頭痛",
   "胃食道逆流",
   "鼻竇炎",
+  "糖尿病",
+  "高血壓",
+  "脂肪肝",
+  "骨質疏鬆症",
+  "關節炎",
+  "憂鬱症",
+  "焦慮症",
+  "便秘",
+  "氣喘",
+  "消化性潰瘍",
+  "蕁麻疹",
+  "濕疹",
+  "痛風",
+  "帶狀皰疹",
+  "甲狀腺功能低下",
+  "中耳炎",
+  "急性支氣管炎",
+  "過敏性鼻炎", // 雖然已有季節性過敏，但過敏性鼻炎更明確
+  "尿道感染",
+  "貧血"
 ];
 
 export const getRandomCondition = (): string => {
   return medicalConditions[Math.floor(Math.random() * medicalConditions.length)];
 };
+
